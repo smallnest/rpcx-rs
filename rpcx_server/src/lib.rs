196f34ad28224673c4615a2e13eeb03e4bf89d05
smallnest <smallnest@gmail.com>
2019-08-07 01:04:59 +0800

    add register_func macro

--- rpcx_server/src/lib.rs
+++ rpcx_server/src/lib.rs
@@ -1,14 +1,46 @@
-use std::any::Any;
+use std::boxed::Box;
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
 
-use rpcx_derive::*;
-use rpcx_protocol::{Error, ErrorKind, Result, RpcxParam, SerializeType};
+use rpcx_protocol::Result;
 
+pub type RpcxFn = fn(&[u8]) -> Result<Vec<u8>>;
 
-#[derive(Debug)]
 pub struct Server {
-    
+    pub services: Arc<RwLock<HashMap<String, Box<RpcxFn>>>>,
 }
 
 impl Server {
+    pub fn new() -> Self {
+        Server {
+            services: Arc::new(RwLock::new(HashMap::new())),
+        }
+    }
+
+    pub fn register_fn(&mut self, service_path: String, service_method: String, f: RpcxFn) {
+        let key = format!("{}.{}", service_path, service_method);
+        let services = self.services.clone();
+        let mut map = services.write().unwrap();
+        map.insert(key, Box::new(f));
+    }
+
+    pub fn get_fn(&self, service_path: String, service_method: String) -> Option<RpcxFn> {
+        let key = format!("{}.{}", service_path, service_method);
+        let map = self.services.read().unwrap();
+        let box_fn = map.get(&key)?;
+        Some(**box_fn)
+    }
 }
 
+#[macro_export]
+macro_rules! register_func {
+    ($rpc_server:expr, $service_path:expr, $service_method:expr, $service_fn:expr, $arg_type:expr, $reply_type:expr) => {{
+        let f: RpcxFn = |x| {
+            let mut args: ArithAddArgs = Default::default();
+            args.from_slice(SerializeType::JSON, x)?;
+            let reply: ArithAddReply = $service_fn(args);
+            reply.into_bytes(SerializeType::JSON)
+        };
+        $rpc_server.register_fn($service_path.to_string(), $service_method.to_string(), f);
+    }};
+}
--- rpcx_server/src/main.rs
+++ rpcx_server/src/main.rs
@@ -7,6 +7,7 @@ use serde::{Deserialize, Serialize};
 
 use rpcx_derive::*;
 use rpcx_protocol::{Error, ErrorKind, Result, RpcxParam, SerializeType};
+use rpcx_server::*;
 
 #[derive(RpcxParam, Default, Debug, Copy, Clone, Serialize, Deserialize)]
 struct ArithAddArgs {
@@ -28,47 +29,20 @@ fn test(args: ArithAddArgs) -> ArithAddReply {
 }
 
 fn main() {
-    let f: RpcxFn = |x| {
-        let mut args: ArithAddArgs = Default::default();
-        args.from_slice(SerializeType::JSON, x)?;
-        let reply: ArithAddReply = test(args);
-        reply.into_bytes(SerializeType::JSON)
-    };
+    let mut rpc_server = Server::new();
+    register_func!(
+        rpc_server,
+        "Arith",
+        "Add",
+        test,
+        ArithAddArgs,
+        ArithAddReply
+    );
 
+    let f = rpc_server
+        .get_fn(String::from("Arith"), String::from("Add"))
+        .unwrap();
     let s = String::from(r#"{"A":1,"B":2}"#);
     let reply = f(s.as_ref()).unwrap();
     println!("reply:{}", String::from_utf8(reply).unwrap());
-}
-
-fn fun_test() {
-    let y = 2;
-    //static dispatch
-    fun_test_impl(5, times2);
-    fun_test_impl(5, |x| 2 * x);
-    fun_test_impl(5, |x| y * x);
-    //dynamic dispatch
-    fun_test_dyn(5, &times2);
-    fun_test_dyn(5, &|x| 2 * x);
-    fun_test_dyn(5, &|x| y * x);
-    //C-like pointer to function
-    fun_test_ptr(5, times2);
-    fun_test_ptr(5, |x| 2 * x); //ok: empty capture set
-                                // fun_test_ptr(5, |x| y*x); //error: expected fn pointer, found closure
-}
-
-fn fun_test_impl(value: i32, f: impl Fn(i32) -> i32) -> i32 {
-    println!("{}", f(value));
-    value
-}
-fn fun_test_dyn(value: i32, f: &dyn Fn(i32) -> i32) -> i32 {
-    println!("{}", f(value));
-    value
-}
-fn fun_test_ptr(value: i32, f: fn(i32) -> i32) -> i32 {
-    println!("{}", f(value));
-    value
-}
-
-fn times2(value: i32) -> i32 {
-    2 * value
 }
